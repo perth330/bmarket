f22f90199c949917c224f84a1a61f368cbb1e641
hirorotaka4 <hirorotaka4@icloud.com>
2020-05-13 19:19:47 +0900

    topPage一覧表示実装
    pickupBrandのview
    pickupCategoryのview

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    @categoryProducts = Product.includes(:images).limit(3).order("id DESC")
+    @categoryBrand = Product.includes(:images).limit(3).order("RAND()")
   end
 end
