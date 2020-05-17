1d232701bb9fa65756c41bbb6778a6c91d0dc62a
perth330 <perth330@hotmail.com>
2020-05-17 23:38:37 +0900

    save

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -37,7 +37,28 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
-  
+
+  def edit
+    @product = Product.find(params[:id])
+    @category = Category.where(params[:category_id])
+    @brand = Brand.find(@product.brand_id)
+    @image = Image.find(params[:id])
+  end
+  def update
+    if brand_params{:name} != ""
+      @brand = Brand.find_by(name: "#{brand_params}")
+      @brand = Brand.create(brand_params) if @brand == nil
+    else
+      @brand = Brand.find(1)
+    end
+    product = Product.find(params[:id])
+    categories = Category.roots
+    product.update(product_create_params)
+    brand = Brand.update(brand_params)
+  end
+
+
+
   private
   def product_create_params
     params.require(:product).permit(:name,:introduction,:size,:category_id,:condition,:delivery_cost,:from,:delivery_day,:price,images_attributes: [:image_url]).merge(user_id:current_user.id,status:"出品中",brand_id:@brand.id)
