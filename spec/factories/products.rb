83c14de3fcabcd35e532d9261e3d966be5a28aa0
DrownedFish74 <drowned.lolita@gmail.com>
2020-05-17 17:12:36 +0900

    Add test

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -10,10 +10,26 @@ FactoryBot.define do
     price           {"500"}
     size            {"S"}
     status          {"出品中"}
-    user_id            {"1"}
-    brand_id           {"1"}
-    category_id        {"1"}
-
+    association :user, factory: :user
+    association :brand, factory: :brand
+    association :category, factory: :category
+    after(:build) do |product|
+      product.images<< build(:image, product: product)
+    end
   end
 
+  factory :product_without_image do
+    name            {"商品名"}
+    introduction    {"説明"}
+    condition       {"新品、未使用"}
+    delivery_cost   {"着払い（購入者負担）"}
+    from            {"北海道"}
+    delivery_day    {"2〜3日で発送"}
+    price           {"500"}
+    size            {"S"}
+    status          {"出品中"}
+    association :user, factory: :user
+    association :brand, factory: :brand
+    association :category, factory: :category
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,128 +1,103 @@
 require 'rails_helper'
 describe Product do
   describe '#create' do
+    let(:user){create(:user)}
+    let(:brand){create(:brand)}
+    let(:category){create(:category)}
     it "nameがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, name: "")
       product.valid?
       expect(product.errors[:name]).to include("を入力してください")
     end
 
     it "introductionがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, introduction: "")
       product.valid?
       expect(product.errors[:introduction]).to include("を入力してください")
     end
 
     it "conditionがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, condition: "")
       product.valid?
       expect(product.errors[:condition]).to include("を入力してください")
     end
 
     it "delivery_costがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, delivery_cost: "")
       product.valid?
       expect(product.errors[:delivery_cost]).to include("を入力してください")
     end
 
     it "fromがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, from: "")
       product.valid?
       expect(product.errors[:from]).to include("を入力してください")
     end
 
     it "delivery_dayがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, delivery_day: "")
       product.valid?
       expect(product.errors[:delivery_day]).to include("を入力してください")
     end
 
     it "priceがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, price: "")
       product.valid?
       expect(product.errors[:price]).to include("は数値で入力してください")
     end
 
     it "statusがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, status: "")
       product.valid?
       expect(product.errors[:status]).to include("を入力してください")
     end
 
     it "user_idがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, user_id: "")
       product.valid?
       expect(product.errors[:user_id]).to include("を入力してください")
     end
 
     it "brand_idがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, brand_id: "")
       product.valid?
       expect(product.errors[:brand_id]).to include("を入力してください")
     end
 
     it "category_idがない場合は登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, category_id: "")
       product.valid?
       expect(product.errors[:category_id]).to include("を入力してください")
     end
 
     it "priceが300未満のときは登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, price: "299")
       product.valid?
       expect(product.errors[:price]).to include("は299より大きい値にしてください")
     end
 
     it "priceが100万以上のときは登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
+
       product = build(:product, price: "1000000")
       product.valid?
       expect(product.errors[:price]).to include("は1000000より小さい値にしてください")
     end
 
     it "画像がないときは登録できないこと" do
-      user = create(:user)
-      brand = create(:brand)
-      category = create(:category)
-      product = build(:product, images: "")
+
+      product = build(:product_without_image)
       product.valid?
       expect(product.errors[:images]).to include("を入力してください")
     end
