b0684fe5ec196cf830a2d44e4dc10a835eb437c1
marine3991 <marine3991@icloud.com>
2020-05-19 10:13:36 +0900

    address-test-commit

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+
+  factory :address do
+    family_name           {"名前"}
+    first_name            {"次郎"}
+    family_name_kana      {"ナマエ"}
+    first_name_kana       {"ジロウ"}
+    prefecture            {"31"}
+    city                  {"大阪市"}
+    town                  {"中央区難波"}
+    town_number           {"5-1-60"}
+    zipcode               {"5420076"}
+  
+  end
+
+end
