7de88a3685db5f8551201212bd25f797d7eab43a
DrownedFish74 <drowned.lolita@gmail.com>
2020-05-19 17:15:29 +0900

    Add buying feature without credit

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -2,17 +2,27 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def new
+    Payjp.api_key = "秘密鍵"
     @product = Product.find(params[:product_id])
-    @credits = Credit.where(user_id:current_user.id)
+    @credit = Payjp::Customer.retrieve(current_user.credit.customer_id).cards.retrieve(current_user.credit.card_id)
     @purchase = Purchase.new
+    @address = current_user.address
   end
 
   def create
+    @product = Product.find(params[:product_id])
+    credit = current_user.credit
     Payjp.api_key = "秘密鍵"
     Payjp::Charge.create(
-      amount: 809, # 決済する値段
-      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
+      amount: @product.price,
+      card: credit.card_id,
       currency: 'jpy'
     )
+    @purchase = Purchase.new(create_purchase)
+  end
+
+  private
+  def create_purchase
+    params.require(:purchase).permit(:address_id).merge(seller_id:@product.user_id,buyer_id:current_user.id)
   end
 end
