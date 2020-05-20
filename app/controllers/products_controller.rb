32ab61d2d86f6a9af93be8c8c8995488703f5e9b
hirorotaka4 <hirorotaka4@icloud.com>
2020-05-20 10:15:19 +0900

    toppageの一覧表示を出品中の物のみに変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,8 @@ class ProductsController < ApplicationController
   before_action :move_to_root, except: [:index, :show]
 
   def index
-    @categoryProducts = Product.includes(:images).limit(3).order("id DESC")
-    @categoryBrand = Product.includes(:images).limit(3).order("RAND()")
+    @categoryProducts = Product.includes(:images).where(status: 0).limit(3).order("id DESC")
+    @categoryBrand = Product.includes(:images).where(status: 0).limit(3).order("RAND()")
   end
   
   def show
@@ -51,4 +51,3 @@ class ProductsController < ApplicationController
     redirect_to new_user_session_path unless user_signed_in?
   end
 end
-
