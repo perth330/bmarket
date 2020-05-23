c68adf597323b10aa9fe62d0b4ce92d90e70395f
hirorotaka4 <hirorotaka4@icloud.com>
2020-05-23 10:58:54 +0900

    credit.rb 単体テスト実施

--- spec/models/credit_spec.rb
+++ spec/models/credit_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Credit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Credit do
+  describe "#create" do
+    let(:user) { create(:user) }
+    # 1
+    it "user_idが空では登録できないこと" do
+      card = build(:credit, user_id: "")
+      card.valid?
+      expect(card.errors[:user_id]).to include("を入力してください")
+    end
+
+    # 2. customer_idが空では登録できないこと
+    it "customer_idが空では登録できないこと" do
+      card = build(:credit, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+
+    # 3. card_idが空では登録できないこと
+    it "card_idが空では登録できないこと" do
+      card = build(:credit, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
+  end
 end
