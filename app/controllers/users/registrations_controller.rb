24bf8a086ac28c6c9cdb40074ac065f7c74c5f99
hirorotaka4 <hirorotaka4@icloud.com>
2020-05-25 21:21:53 +0900

    user及びaddressをウィザード形式で登録

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,31 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = { user: @user.attributes }
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.addresses.build
+    render :new_address
+  end
+
+  def create_address
+    @user = User.new(session["devise.regist_data"]["user"])
+    @address = Address.new(address_params)
+    unless @address.valid?
+      flash.now[:alert] = @address.errors.full_messages
+      render :new_address and return
+    end
+    @user.addresses.build(@address.attributes)
+    @user.save
+    session["devise.regist_data"]["user"].clear
+    sign_in(:user, @user)
+    redirect_to root_path, notice: "ユーザー情報及び住所登録が完了しました。"
+  end
 
   # GET /resource/edit
   # def edit
@@ -38,7 +60,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def address_params
+    params.require(:address).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :zipcode, :prefecture, :city, :town, :town_number, :building, :tel)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
