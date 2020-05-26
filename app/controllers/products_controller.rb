44ce9b13fcf91a6c4628db010b36a13885dbdf4d
marine3991 <marine3991@icloud.com>
2020-05-26 16:46:50 +0900

    favorite sever commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,6 +31,7 @@ class ProductsController < ApplicationController
   end
   
   def show
+    @favorite = Favorite.find_by(user_id: current_user.id, product_id: params[:product_id])
     if user_signed_in?
       @addresses = Address.where(user_id:current_user.id)
       @credit = Credit.where(user_id:current_user.id)
