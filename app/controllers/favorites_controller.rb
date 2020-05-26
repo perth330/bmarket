06f815652aff2b06273e36c1f599f684cfb7e01d
marine3991 <marine3991@icloud.com>
2020-05-26 18:41:56 +0900

    css fav2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,23 +6,23 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    return nil if user.signed_in?
-    return nil if @product.user_id == current_user.id or Favorite.find_by(product_id:@product.id,user_id:current_user.id) != nil
-    @favorite = Favorite.create(user_id: current_user.id, product_id: params[:product_id])
+    # return nil if user.signed_in?
+    # return nil if @product.user_id == current_user.id or Favorite.find_by(product_id:@product.id,user_id:current_user.id) != nil
+    @favorite = Favorite.create(user_id: current_user.id, product_id: @product.id)
     @favorites = Favorite.where(product_id: params[:product_id])
+    redirect_to request.referer
   end
 
   def destroy
-    return nil if user.signed_in?
-    return nil if @product.user_id == current_user.id or Favorite.find_by(product_id:@product.id,user_id:current_user.id) == nil
-    @favorite = Favorite.find_by(user_id: current_user.id, product_id: params[:id])
+    # return nil if user.signed_in?
+    # return nil if @product.user_id == current_user.id or Favorite.find_by(product_id:@product.id,user_id:current_user.id) == nil
+    @favorite = Favorite.find_by(user_id: current_user.id, product_id: @product.id)
     @favorite.destroy
     redirect_to request.referer
   end
 
   def set_item
-    @product = Product.find(params[:id])
-    @products = Product.includes(:images).order(:product_purchase_id, "id DESC")
+    @product = Product.find(params[:format])
   end
 
 
