4c542740583157897c2112bd80c71825f8c77e4d
marine3991 <marine3991@icloud.com>
2020-05-27 10:24:52 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_135404) do
     t.string "from", null: false
     t.string "delivery_day", null: false
     t.bigint "price", null: false
-    t.string "size", null: false
+    t.string "size"
     t.string "status", null: false
     t.bigint "user_id", null: false
     t.bigint "brand_id", null: false
@@ -92,16 +92,16 @@ ActiveRecord::Schema.define(version: 2020_05_25_135404) do
   end
 
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "seller_id_id"
-    t.bigint "buyer_id_id"
+    t.bigint "seller_id"
+    t.bigint "buyer_id"
     t.bigint "product_id"
     t.bigint "address_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["address_id"], name: "index_purchases_on_address_id"
-    t.index ["buyer_id_id"], name: "index_purchases_on_buyer_id_id"
+    t.index ["buyer_id"], name: "index_purchases_on_buyer_id"
     t.index ["product_id"], name: "index_purchases_on_product_id"
-    t.index ["seller_id_id"], name: "index_purchases_on_seller_id_id"
+    t.index ["seller_id"], name: "index_purchases_on_seller_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -132,6 +132,6 @@ ActiveRecord::Schema.define(version: 2020_05_25_135404) do
   add_foreign_key "products", "users"
   add_foreign_key "purchases", "addresses"
   add_foreign_key "purchases", "products"
-  add_foreign_key "purchases", "users", column: "buyer_id_id"
-  add_foreign_key "purchases", "users", column: "seller_id_id"
+  add_foreign_key "purchases", "users", column: "buyer_id"
+  add_foreign_key "purchases", "users", column: "seller_id"
 end
