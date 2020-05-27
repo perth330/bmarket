91185d1c4cfed100cc75b1f98ec5be8b7d14fdd5
yassan2345 <y234.s678.jobs@gmail.com>
2020-05-27 16:14:20 +0900

    不要な記述の削除

--- app/controllers/infomations_controller.rb
+++ app/controllers/infomations_controller.rb
@@ -3,11 +3,9 @@ class InfomationsController < ApplicationController
   before_action :set_products,only:[:index,:show]
 
   def index
-    @products = Product.includes(:images).where(user_id: current_user.id).where(status: 0).order("id DESC")
   end
 
   def show
-    @products = Product.includes(:images).where(user_id: current_user.id).where(status: 1).order("id DESC")
   end
 
   private
