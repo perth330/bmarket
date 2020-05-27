21c929ceab9952436da2c558e32e508a47a07017
perth330 <perth330@hotmail.com>
2020-05-27 16:35:43 +0900

    update infomations controller

--- app/controllers/infomations_controller.rb
+++ app/controllers/infomations_controller.rb
@@ -1,17 +1,15 @@
 class InfomationsController < ApplicationController
   before_action :redirect_root,only:[:index,:show]
-  before_action :set_products,only:[:index,:show]
 
   def index
+    @products = Product.includes(:images).where(user_id: current_user.id).where(status: 0).order("id DESC")
   end
 
   def show
+    @products = Product.includes(:images).where(user_id: current_user.id).where(status: 1).order("id DESC")
   end
 
   private
-  def set_products
-    @products = Product.includes(:images).where(user_id: current_user.id).where(status: 0).order("id DESC")
-  end
 
   def redirect_root
     redirect_to root_path unless user_signed_in?
